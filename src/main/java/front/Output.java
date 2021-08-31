696373db5dfb89c24a09c4106fc2752b5a2ecd61
Ignacio <isampedro@itba.edu.ar>
2021-08-31 19:24:09 -0300

    Now, the colors depends solely of the radius

--- src/main/java/front/Output.java
+++ src/main/java/front/Output.java
@@ -109,13 +109,13 @@ public class Output {
         	writer.write(cells.size() +"\n\n");
         	if(cells.iterator().next().getDimension() == 2)
         	{
-        		boardRadius = Math.sqrt(Math.pow(boardSize, 2) * 2) / 2;
+        		boardRadius = Math.sqrt(Math.pow(boardSize/(float)2, 2) * 2);
             	for(Cell c : cells)
             		writer.write(c.getX() +"\t" +c.getY() +"\t" + (c.getState() == State.DEAD ? 1:0) + "\t" + getRadius(c, boardSize)/boardRadius + "\t" + (1 - getRadius(c, boardSize)/boardRadius) + "\n");
         	}
         	else
         	{
-        		boardRadius = Math.sqrt(Math.pow(boardSize, 2) * 3);
+        		boardRadius = Math.sqrt(Math.pow(boardSize/(float)2, 2) * 3);
             	for(Cell c : cells)
             		writer.write(c.getX() +"\t" +c.getY() +"\t" +c.getZ() +"\t" + (c.getState() == State.DEAD ? 1:0) + "\t" + (getRadius(c, boardSize)/boardRadius) + "\t" + (1 - getRadius(c, boardSize)/boardRadius) + "\n");
         	}
